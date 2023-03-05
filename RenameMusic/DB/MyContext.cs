79c15307a721cc3cb9d1f2039c8d95c86a13c6c3
Ignacio <42973262+IgnacioVeiga@users.noreply.github.com>
2023-03-05 02:28:00 -0300

    Some Database functions moved

--- RenameMusic/DB/MyContext.cs
+++ RenameMusic/DB/MyContext.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.IO;
-using WinCopies.Linq;
 
 namespace RenameMusic.DB
 {
@@ -17,112 +16,7 @@ namespace RenameMusic.DB
             {
                 Directory.CreateDirectory(databaseFolder);
             }
-
-            // P: Primary S: Secondary F: Folder
-            optionsBuilder.UseSqlite($"Data Source={databaseFolder}PSF_List.db");
-        }
-
-        public int AddAudioToDB(AudioDTO audio)
-        {
-            using (MyContext context = new())
-            {
-                // Crea la base de datos si no existe
-                context.Database.EnsureCreated();
-
-                // Agrega un nuevo audio
-                var resp = context.Audios.Add(audio);
-                context.SaveChanges();
-                return resp.Entity.Id;
-            }
-        }
-
-        public int AddFolderToDB(FolderDTO folder)
-        {
-            using (MyContext context = new())
-            {
-                // Crea la base de datos si no existe
-                context.Database.EnsureCreated();
-
-                // Agrega un nuevo audio
-                var resp = context.Folders.Add(folder);
-                context.SaveChanges();
-                return resp.Entity.Id;
-            }
-        }
-
-        public void RemoveAudioFromDB(int id)
-        {
-            using (MyContext context = new())
-            {
-                AudioDTO audioToRemove = context.Audios.FirstPredicate(a => a.Id == id);
-                context.Audios.Remove(audioToRemove);
-
-                if (!context.Folders.AnyPredicate(f => f.Id == audioToRemove.FolderId))
-                {
-                    RemoveFolderFromDB(audioToRemove.FolderId);
-                }
-
-                context.SaveChanges();
-            }
-        }
-
-        public void RemoveFolderFromDB(int folderId)
-        {
-            using (MyContext context = new())
-            {
-                foreach (AudioDTO item in context.Audios.WherePredicate(a => a.FolderId == folderId))
-                {
-                    context.Audios.Remove(item);
-                }
-
-                context.Folders.Remove(context.Folders.FirstPredicate(a => a.Id == folderId));
-                context.SaveChanges();
-            }
-        }
-
-        public bool AudioAlreadyAdded(string filepath)
-        {
-            using (MyContext context = new())
-            {
-                // Crea la base de datos si no existe
-                context.Database.EnsureCreated();
-
-                return context.Audios.AnyPredicate(a => a.FilePath == filepath);
-            }
-        }
-
-        // ToDo: En caso de ser verdadero deberia retornar el bool y el id
-        public bool FolderAlreadyAdded(string folderpath)
-        {
-            using (MyContext context = new())
-            {
-                // Crea la base de datos si no existe
-                context.Database.EnsureCreated();
-
-                return context.Folders.AnyPredicate(f => f.FolderPath == folderpath);
-            }
-        }
-
-        public int GetAudioId(string filepath)
-        {
-            using (MyContext context = new())
-            {
-                // Crea la base de datos si no existe
-                context.Database.EnsureCreated();
-
-                return context.Audios.FirstPredicate(a => a.FilePath == filepath).Id;
-            }
-        }
-
-        public int GetFolderId(string folderpath)
-        {
-            using (MyContext context = new())
-            {
-                // Crea la base de datos si no existe
-                context.Database.EnsureCreated();
-
-                return context.Folders.FirstPredicate(a => a.FolderPath == folderpath).Id;
-            }
+            optionsBuilder.UseSqlite($"Data Source={databaseFolder}List.db");
         }
     }
 }
--- RenameMusic/Util/ListManager.cs
+++ RenameMusic/Util/ListManager.cs
@@ -1,62 +1,177 @@
 ﻿using RenameMusic.DB;
+using System;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
+using WinCopies.Linq;
 
 namespace RenameMusic.Util
 {
     public static class ListManager
     {
-        public static void AddItemsToListView()
-        {
-            //Audio audio = new(folder.Id, file);
-            //if (audio.Tags is not null)
-            //{
-            //    if (string.IsNullOrEmpty(audio.NewName))
-            //        secondaryList.Items.Add(audio);
-            //    else
-            //        primaryList.Items.Add(audio);
-            //}
+        //public async static void FromDatabaseToListView(int pageSize)
+        //{
+        //    LoadingBar loadingBar = new(pageSize);
+        //    loadingBar.Show();
+        //    // Desde la base de datos se debe retornar una lista pequeña para cada una de las 3 listas.
+        //    // Esto último debe funcionar como las páginas de un libro, con la posibilidad de elegir la
+        //    // cantidad de elementos a mostrar por cada página.
 
-            // Desde la base de datos se debe retornar una lista pequeña para cada una de las 3 listas.
-            // Esto último debe funcionar como las páginas de un libro, con la posibilidad de elegir la
-            // cantidad de elementos a mostrar por cada página.
-        }
+        //    await Task.Run(() =>
+        //    {
+        //        using (MyContext context = new())
+        //        {
+        //        }
+
+        //        loadingBar.Dispatcher.Invoke(() => loadingBar.UpdateProgress());
+        //    });
+
+        //    loadingBar.Close();
+        //}
 
         public async static void AddToDatabase(string[] files)
         {
             LoadingBar loadingBar = new(files.Length);
             loadingBar.Show();
-            MyContext myContext = new();
 
             await Task.Run(() =>
             {
                 foreach (string file in files)
                 {
+                    // Crea la base de datos si no existe
+                    _ = new MyContext().Database.EnsureCreatedAsync();
+
                     // Chequear si ya existen el audio o la carpeta en la base de datos
-                    if (myContext.AudioAlreadyAdded(file))
+                    if (AudioAlreadyAdded(file))
                     {
                         continue;
                     }
 
                     int folderId;
                     string folderPath = Path.GetDirectoryName(file) + Path.DirectorySeparatorChar;
-                    if (myContext.FolderAlreadyAdded(folderPath))
+                    if (FolderAlreadyAdded(folderPath))
                     {
-                        folderId = myContext.GetFolderId(folderPath);
+                        folderId = GetFolderId(folderPath);
                     }
                     else
                     {
-                        folderId = myContext.AddFolderToDB(new FolderDTO() { FolderPath = folderPath });
+                        folderId = AddFolderToDB(new FolderDTO() { FolderPath = folderPath });
                     }
 
-                    myContext.AddAudioToDB(new AudioDTO() { FilePath = file, FolderId = folderId });
+                    AddAudioToDB(new AudioDTO() { FilePath = file, FolderId = folderId });
 
                     loadingBar.Dispatcher.Invoke(() => loadingBar.UpdateProgress());
                 }
             });
 
-            await myContext.DisposeAsync();
             loadingBar.Close();
+        }
+
+        public static int AddAudioToDB(AudioDTO audio)
+        {
+            using (MyContext context = new())
+            {
+                // Crea la base de datos si no existe
+                context.Database.EnsureCreated();
+
+                // Agrega un nuevo audio
+                var resp = context.Audios.Add(audio);
+                context.SaveChanges();
+                return resp.Entity.Id;
+            }
+        }
+
+        public static int AddFolderToDB(FolderDTO folder)
+        {
+            using (MyContext context = new())
+            {
+                // Crea la base de datos si no existe
+                context.Database.EnsureCreated();
+
+                // Agrega un nuevo audio
+                var resp = context.Folders.Add(folder);
+                context.SaveChanges();
+                return resp.Entity.Id;
+            }
+        }
+
+        public static void RemoveAudioFromDB(int id)
+        {
+            using (MyContext context = new())
+            {
+                AudioDTO audioToRemove = context.Audios.FirstPredicate(a => a.Id == id);
+                context.Audios.Remove(audioToRemove);
+
+                if (!context.Folders.AnyPredicate(f => f.Id == audioToRemove.FolderId))
+                {
+                    RemoveFolderFromDB(audioToRemove.FolderId);
+                }
+
+                context.SaveChanges();
+            }
+        }
+
+        public static void RemoveFolderFromDB(int folderId)
+        {
+            using (MyContext context = new())
+            {
+                foreach (AudioDTO item in context.Audios.WherePredicate(a => a.FolderId == folderId))
+                {
+                    context.Audios.Remove(item);
+                }
+
+                context.Folders.Remove(context.Folders.FirstPredicate(a => a.Id == folderId));
+                context.SaveChanges();
+            }
+        }
+
+        public static bool AudioAlreadyAdded(string filepath)
+        {
+            using (MyContext context = new())
+            {
+                return context.Audios.AnyPredicate(a => a.FilePath == filepath);
+            }
+        }
+
+        // ToDo: En caso de ser verdadero deberia retornar el bool y el id
+        public static bool FolderAlreadyAdded(string folderpath)
+        {
+            using (MyContext context = new())
+            {
+                return context.Folders.AnyPredicate(f => f.FolderPath == folderpath);
+            }
+        }
+
+        public static int GetAudioId(string filepath)
+        {
+            using (MyContext context = new())
+            {
+                return context.Audios.FirstPredicate(a => a.FilePath == filepath).Id;
+            }
+        }
+
+        public static int GetFolderId(string folderpath)
+        {
+            using (MyContext context = new())
+            {
+                return context.Folders.FirstPredicate(a => a.FolderPath == folderpath).Id;
+            }
+        }
+
+        public static int CountAudioItems()
+        {
+            using (MyContext context = new())
+            {
+                return context.Audios.Count();
+            }
+        }
+
+        public static int CountFolderItems()
+        {
+            using (MyContext context = new())
+            {
+                return context.Folders.Count();
+            }
         }
     }
 }
