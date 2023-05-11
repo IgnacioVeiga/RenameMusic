a5e3551c85c758363bf037fe050500ac57677bb5
Ignacio <42973262+IgnacioVeiga@users.noreply.github.com>
2023-05-11 20:23:53 -0300

    Fix tag required checked message

--- RenameMusic/Assets/RenamingRule.xaml.cs
+++ RenameMusic/Assets/RenamingRule.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using RenameMusic.Lang;
 using RenameMusic.Properties;
 using RenameMusic.Util;
-using System.Linq;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -12,7 +12,8 @@ namespace RenameMusic.Assets
     /// </summary>
     public partial class RenamingRule : Window
     {
-        readonly string[] tags = { "<tn>", "<t>", "<a>", "<aAt>", "<At>", "<yr>" };
+        readonly string[] ALL_TAGS = { "<tn>", "<t>", "<a>", "<aAt>", "<At>", "<yr>" };
+        private List<string> RequiredTags = new();
 
         public RenamingRule()
         {
@@ -49,7 +50,7 @@ namespace RenameMusic.Assets
             Close();
         }
 
-        // ToDo: mostar el mensaje de "Warning" solo cuando sea necesario
+        // ToDo: mostar el mensaje de "Warning" solo cuando sea necesario y refactorizar
         private void Rule_TextChanged(object sender, TextChangedEventArgs e)
         {
             // No debe ser un espacio vacio o similar
@@ -60,20 +61,19 @@ namespace RenameMusic.Assets
                 return;
             }
 
-            // Debe contener por lo menos 1 tag
-            // ToDo: también deben contener todos los marcados como obligatorios
-            ApplyBTN.IsEnabled = tags.Any(tag => renamingRule.Text.Contains(tag));
+            // Debe contener por lo menos 1 "tag" de los marcados como requeridos
+            ApplyBTN.IsEnabled = RequiredTags.TrueForAll(tag => renamingRule.Text.Contains(tag));
             if (!ApplyBTN.IsEnabled)
             {
                 // ToDo: cambiar el mensaje
-                WarningMSG.Text = $"{Strings.NOT_ALLOWED}: Debe contener un tag minimo";
+                WarningMSG.Text = $"{Strings.NOT_ALLOWED}: Debe contener todos los tags marcados como requeridos";
                 return;
             }
 
             // Reemplazo solo los caracteres invalidos de los "tags", entonces cualquier otro caracter
             // invalido restante no forma parte de los "tags" y esto influye en el botón de aplicar
             string temp = renamingRule.Text;
-            foreach (string tag in tags)
+            foreach (string tag in ALL_TAGS)
             {
                 temp = temp.Replace(tag, tag.Replace("<", "_").Replace(">", "_"));
             }
@@ -86,6 +86,24 @@ namespace RenameMusic.Assets
             else
             {
                 ApplyBTN.IsEnabled = true;
+                WarningMSG.Text = "";
+            }
+        }
+
+        private void TagRequired_CheckChanged(object sender, RoutedEventArgs e)
+        {
+            RequiredTags.Clear();
+            if (titleRequired.IsChecked == true) RequiredTags.Add("<t>");
+            if (albumRequired.IsChecked == true) RequiredTags.Add("<a>");
+            if (albumArtistRequired.IsChecked == true) RequiredTags.Add("<aAt>");
+            if (artistRequired.IsChecked == true) RequiredTags.Add("<At>");
+
+            if (!RequiredTags.TrueForAll(tag => renamingRule.Text.Contains(tag)))
+            {
+                WarningMSG.Text = $"{Strings.NOT_ALLOWED}: Debe contener todos los tags marcados como requeridos";
+            }
+            else
+            {
                 WarningMSG.Text = "";
             }
         }
